4199b99a7cadf1d615648f2e558b4d4969e3cec1
Aayush aryan <aayush.aryan91@gmail.com>
2017-06-23 11:51:26 +0530

    Update Boston_lm.R

--- 2. Linear Regression/2.1 The Boston Dataset/Boston_lm.R
+++ 2. Linear Regression/2.1 The Boston Dataset/Boston_lm.R
@@ -22,10 +22,10 @@
 # 2. DIVIDING DATASET
 #--------------------
   set.seed(1)
-  library(caTools) # sample.split function is present in this package
+  library(caTools) # sample.split function is present in this package.
   
   split_data <- sample.split(Boston$medv, SplitRatio = 0.7)
-  # We divide the data in the ratio 0.7
+  # We divide the data in the ratio 0.7.
   split_data
   
   training_data <- subset(Boston, split_data == "TRUE")
@@ -34,28 +34,28 @@
 #--------------------------------------------------------------------------------------------------------------------------------------#
 # 3. EXPLORATORY ANALYSIS
 #------------------------
+  # Plotting a scatter plot matrix of predictors.
   library(lattice)
-  jpeg('scatterplot1.jpg') # To save plot in a jpeg format
-  splom(~Boston[c(1:6, 14)], groups = NULL, data = Boston, axis.line.tck = 0,
-        axis.text.alpha = 0)
-  dev.off() # It is required to turn off the graphics device
+  jpeg('scatterplot1.jpg') # To save plot in a jpeg format.
+  splom(~Boston[c(1:6, 14)], groups = NULL, data = Boston, axis.line.tck = 0, axis.text.alpha = 0)
+  dev.off() # It is required to turn off the graphics device.
   jpeg('scatterplot2.jpg')
-  splom(~Boston[c(7:14)], groups = NULL, data = Boston,axis.line.tck = 0,
-        axis.text.alpha = 0)
+  splom(~Boston[c(7:14)], groups = NULL, data = Boston,axis.line.tck = 0, axis.text.alpha = 0)
   dev.off()
-  # Checking for "CORRELATION" among data
-  # Correlation is an important factor to check the dependencies among themselves
-  # Correlation analysis gives us an insight for mutual relationship among variables
+
+  # Checking for "CORRELATION" among data.
+  # Correlation is an important factor to check the dependencies among themselves.
+  # Correlation analysis gives us an insight for mutual relationship among variables.
   cr <- cor(Boston)
-  cr # This will give us the correlation coefficients
-  # To visualise this corrplot library is required
+  cr # This will give us the correlation coefficients.
+  # To visualise this "corrplot" library is required.
   library("corrplot")
   jpeg('corrplot.jpg')
   corrplot(cr, type = "lower")
   dev.off()
   
-  # Checking for "MULTICOLLINEARITY"
-  # Multicollinearity exists when two or more predictors are highly correlated among themselves
+  # Checking for "MULTICOLLINEARITY".
+  # Multicollinearity exists when two or more predictors are highly correlated among themselves.
   library("car")
   model <- lm(medv~., data = training_data)
   vif(model) # vif = Variance Inflation Factor
@@ -64,8 +64,8 @@
   # A vif of 1 indicates that there is no correlation among variables.
   # A high vif indicates high multicollinearity.
   
-  # The equation for the best fit line
-  summary(model) # p-value for "indus" is the highest
+  # The search for the equation of the best fit line.
+  summary(model) # p-value for "indus" is the highest.
   # This means that in presence of other predictors, "indus" is highly correlated.
   # P-value tests the NULL hypothesis that coefficient is equal to zero.
   # A high P-value denotes coefficient is really zero.
