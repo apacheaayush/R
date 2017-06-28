ffe85f187484a702a3a3a09244f290cad0d17282
Aayush aryan <aayush.aryan91@gmail.com>
2017-06-29 00:27:12 +0530

    Update dataTablePractise.R

--- 1. R Basics/Data manipulation - Case Study/dataTablePractise.R
+++ 1. R Basics/Data manipulation - Case Study/dataTablePractise.R
@@ -1,10 +1,10 @@
-#---------------------------------------------------------------------------------
-#---------------------------------------------------------------------------------
-#--------------------------------------------------------------------------------
-#                 "*Some basic Data Manipulation problems in R*"
-#--------------------------------------------------------------------------------
-#--------------------------------------------------------------------------------------------
-#--------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------------------------------#
+#                                   "*Some basic Data Manipulation problems in R*"
+#---------------------------------------------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------------------------------#
     
     # Question 1: Import the customer data into R using read.csv, read.table etc.
       # CODE :-
@@ -16,10 +16,11 @@
         customers <- as.data.table(customers)
         summary(customers)
         
-#-----------
-#-----------
+#----------------------------------------------------------------------------------------------------------------------------------------#
+#----------------------------------------------------------------------------------------------------------------------------------------#
     
-    # Question 2: Understand the data using different functions like View, head, tail, str, names, nrow, ncol, summary, duplicates, describe etc.
+    # Question 2: Understand the data using different functions like View, head, tail, str, names, nrow, ncol, summary, duplicates, 
+    # describe etc.
       # CODE :-
         
         # 1. View function
@@ -51,11 +52,12 @@
              describe(customers)
              
         # 8. Duplicate observations in the dataset.
-             anyDuplicated(customers, by = "Customer.ID") # Result - 9818. 9818 is the index of the first duplicate, if there would be no duplicates this value would be 0
+             anyDuplicated(customers, by = "Customer.ID") # Result - 9818. 9818 is the index of the first duplicate, if there would
+                                                          # be no duplicates this value would be 0.
              duplicated(customers) # Returns a logical vector indicating which rows are duplicates.
              
-#--------------
-#--------------
+#-------------------------------------------------------------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------------------------#
              
     # Question 3: What is percentage of missing values for customer value variable?
       # CODE :-
@@ -69,8 +71,8 @@
         # Percentage of missing values for customer value variable.
           (sum(is.na(customers$Customer.Value))/nrow(customers))*100
           
-#------------
-#------------
+#-------------------------------------------------------------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------------------------#
           
     # Question 4 : Create two subsets with unique and duplicate values.
       # CODE :-
@@ -79,26 +81,27 @@
         duplicatecustomers <- customers[duplicated(customers, by = "Customer.ID") == TRUE,]     
         View(duplicatecustomers)
         
-#------------
-#------------
+#--------------------------------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------------------------------#
         
     # Question 5: Create data set with list of customers whose customer value is greater than 10000.
       # CODE :-
         customers[Customer.Value > 10000]
         
-#------------
-#------------
+#--------------------------------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------------------------------#
         
     # Question 6: In customer table, create a new variable called "customer value segment" using customer value as follows.
     #             - High Value Segment - > 25000,
     #             - Medium Value Segment - Between 10000 and 25000,
     #             - Low Value Segment - less than or equal to 10000.
       # CODE :-
-        customers[ , customerValueSegment := ifelse(Customer.Value > 25000, "High Value Segment", ifelse(Customer.Value < 10000, "Low Value Segment", "Medium Value Segment"))]
+        customers[ , customerValueSegment := ifelse(Customer.Value > 25000, "High Value Segment", ifelse(Customer.Value < 10000, 
+                     "Low Value Segment", "Medium Value Segment"))]
         View(customers)
         
-#------------
-#------------
+#--------------------------------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------------------------------#
         
     # Question 7: Create variables "average revenue per trip" and "balance points" in the customer data set.
       # CODE :-
@@ -106,8 +109,8 @@
         customers[, "BalancePoints" := (Points.earned - Points.redeemed)]
         View(customers)
         
-#------------
-#------------
+#---------------------------------------------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------------------------------#
         
     # Question 8: How many days between last purchase date and today?
       # CODE :-
@@ -115,8 +118,8 @@
         customers$recent.date <- as.Date(customers$recent.date, format = "%Y%m%d")
         customers[, "lastPurchaseDate" := difftime(recent.date, Sys.Date(), units = "days")]
         
-#------------
-#------------
+#---------------------------------------------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------------------------------#
         
     # Question 9: Calculate percentage of sales by last city, state and region.
       # CODE :-
@@ -124,8 +127,8 @@
         View(SumValue)
         SumValue[, "percentSales" := ((V1/sum(V1))*100)]
         View(SumValue) 
-#------------
-#------------
+#---------------------------------------------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------------------------------#
         
     # Question 10: What is the count of customers and average number of purchases by last state and city.
       # CODE :-
@@ -135,6 +138,6 @@
         a <- Count[, sum(buy.times), by = list(Last_state, Last_city)] # Buy times by Last state & city.
         a <- (a/sum(customers$buy.times))
     
-#------------
-#------------
+#---------------------------------------------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------------------------------#
         
